b734b99a301833d6d33d88b5bcf2660f6b1a40f1
TNTSuperMan <tntsuperga@gmail.com>
2024-03-08 22:06:15 +0900

    文字置き換えプラグイン

---
+++ plugin/omit/script.js
@@ -0,0 +1,24 @@
+export const mode = "text";
+export let config;
+export function set(a){
+    config = a;
+}
+export function func(e){
+    const ryexp = /om[[\w+-.]+]/;
+    let ret = Array();
+    e.forEach(t=>{
+        if(!ryexp.test(t)) {
+            ret.push(t);
+            return;
+        }else{
+            console.log(config);
+            config.forEach(c=>{
+                if(("om[" + c.id + "]") === t){
+                    ret.push(c.data);
+                    return;
+                }
+            })
+        }
+    })
+    return ret;
+};
